83f94e1fb75e08b3eb3fa3687caa9f0653814e80
Pailin <klaw.amz@gmail.com>
2017-11-29 19:05:07 +0700

    Add google sign-in.

--- app/src/main/java/com/pailin_rungruang/myrtdbapplication/MainActivity.java
+++ app/src/main/java/com/pailin_rungruang/myrtdbapplication/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.ads.internal.gmsg.HttpClient;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -27,6 +28,7 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -36,7 +38,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private static final int RC_SIGN_IN = 590;
     private DatabaseReference mRootRef; /*= FirebaseDatabase.getInstance().getReference();*/
@@ -50,23 +52,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SignInButton SignIn;
     private TextView Name, Email;
     private ImageView Pic;
-    private GoogleApiClient mGoogleSignInClient;
+    private GoogleSignInClient mGoogleSignInClient;
 
     private Button btnSaveRecord;
     private Button btnDisplayRecord;
 
-    private void onViewBind(){
 
-        btnSaveRecord = (Button)findViewById(R.id.btn_save);
-        btnDisplayRecord = (Button)findViewById(R.id.btn_display);
+    private void onViewBind() {
+
+        btnSaveRecord = (Button) findViewById(R.id.btn_save);
+        btnDisplayRecord = (Button) findViewById(R.id.btn_display);
 
         // Google Sign In
-        Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
-        SignOut = (Button)findViewById(R.id.btn_logout_google_account);
+        Prof_Section = (LinearLayout) findViewById(R.id.prof_section);
+        SignOut = (Button) findViewById(R.id.btn_logout_google_account);
         SignIn = (SignInButton) findViewById(R.id.sign_in_button);
-        Name = (TextView)findViewById(R.id.tv_display_name);
-        Email = (TextView)findViewById(R.id.tv_display_email);
-        Pic = (ImageView)findViewById(R.id.iv_display_image);
+        Name = (TextView) findViewById(R.id.tv_display_name);
+        Email = (TextView) findViewById(R.id.tv_display_email);
+        Pic = (ImageView) findViewById(R.id.iv_display_image);
     }
 
     @Override
@@ -83,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Prof_Section.setVisibility(View.GONE);
 
 
+
+
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -90,11 +95,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .build();
 
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
 
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        checkAlreadySigned();
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true); //set Offline mode
         mRootRef = FirebaseDatabase.getInstance().getReference();
@@ -105,7 +110,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //        mAuth = FirebaseAuth.getInstance();
 
+    }
 
+    public void sendIdToken(){
+        // ID token to my server
+//        HttpClient httpClient = new DefaultHttpClient();
     }
 
     public void saveRecord() {
@@ -118,7 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String stringMessage = etMessage.getText().toString();
         String stringUsername = etUsername.getText().toString();
 
-        if(stringMessage.isEmpty() || stringUsername.isEmpty()){
+        if (stringMessage.isEmpty() || stringUsername.isEmpty()) {
             return;
         }
 
@@ -153,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String stringMessage = dataSnapshot.child("records").child("message").getValue().toString();
                 String stringUsername = dataSnapshot.child("records").child("username").getValue().toString();
 
-                tvDisplay.setText(stringMessage+"\n"+stringUsername+" ..ไม่ได้กล่าวไว้");
+                tvDisplay.setText(stringMessage + "\n" + stringUsername + " ..ไม่ได้กล่าวไว้");
             }
 
             @Override
@@ -166,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.sign_in_button:
                 signIn();
                 break;
@@ -188,10 +197,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-//        updateUI(account);
+//         Check for existing Google Sign In account, if the user is already signed in
+//         the GoogleSignInAccount will be non-null.
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            updateUI(true);
+        } else {
+            updateUI(false);
+        }
     }
 
     @Override
@@ -199,44 +213,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void signIn(){
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+    private void signIn() {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(false);
+                    }
+                });
+
+//        Auth.GoogleSignInApi.signOut(mGoogleSignInClient).setResultCallback(new ResultCallback<Status>() {
+//            @Override
+//            public void onResult(@NonNull Status status) {
+//                updateUI(false);
+//            }
+//        });
 
     }
 
-    private void signOut(){
-        Auth.GoogleSignInApi.signOut(mGoogleSignInClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                updateUI(false);
-            }
-        });
-
+    private void revokeAccess(){
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(false);
+                    }
+                });
     }
 
-    private void handleResult(GoogleSignInResult result){
-        if(result.isSuccess()){
-            GoogleSignInAccount account = result.getSignInAccount();
-            String name = account.getDisplayName();
-            String email = account.getEmail();
-            String img_url = account.getPhotoUrl().toString();
-
-            Name.setText(name);
-            Email.setText(email);
-
-            Glide.with(this).load(img_url).into(Pic);
-
-            updateUI(true);
-        }
-
+    private void checkAlreadySigned(){
+        mGoogleSignInClient.silentSignIn()
+                .addOnCompleteListener(this, new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        handleSignInResult(task);
+                    }
+                });
     }
 
-    private void updateUI(boolean isLogin){
-        if(isLogin){
+//    private void handleResult(GoogleSignInResult result){
+//        if(result.isSuccess()){
+//            GoogleSignInAccount account = result.getSignInAccount();
+//            String name = account.getDisplayName();
+//            String email = account.getEmail();
+//            String img_url = account.getPhotoUrl().toString();
+//
+//            Name.setText(name);
+//            Email.setText(email);
+//
+//            Glide.with(this).load(img_url).into(Pic);
+//
+//            updateUI(true);
+//        }
+//
+//    }
+
+    private void updateUI(boolean isLogin) {
+        if (isLogin) {
             Prof_Section.setVisibility(View.VISIBLE);
             SignIn.setVisibility(View.GONE);
-        }else {
+        } else {
             Prof_Section.setVisibility(View.GONE);
             SignIn.setVisibility(View.VISIBLE);
         }
@@ -246,9 +290,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==RC_SIGN_IN){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleResult(result);
+        if (requestCode == RC_SIGN_IN) {
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            handleResult(result);
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
     }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+                String idToken = account.getIdToken();
+                String name = account.getDisplayName();
+                String email = account.getEmail();
+                String img_url = account.getPhotoUrl().toString();
+                Name.setText(name);
+                Email.setText(email);
+
+                Glide.with(this).load(img_url).into(Pic);
+
+                updateUI(true);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            updateUI(false);
+        }
+
+    }
+
 }
